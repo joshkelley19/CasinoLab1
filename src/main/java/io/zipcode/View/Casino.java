71e19509d7c23502400cc2dccb460e3af680cae8
Adam Kamsheh <adam@Adam-ZCW-2.local>
2016-10-13 15:22:43 -0400

    added '.idea/vcs.xml' to .ignore file

--- src/main/java/io/zipcode/View/Casino.java
+++ src/main/java/io/zipcode/View/Casino.java
@@ -1,6 +1,7 @@
 package io.zipcode.View;
 
 import io.zipcode.Model.Engine;
+import io.zipcode.Model.InvalidBetException;
 
 /**
  * Created by joshuakelley on 10/11/16.
@@ -8,71 +9,71 @@ import io.zipcode.Model.Engine;
 public class Casino {
     Engine engine = new Engine();
     UserInput ui = new UserInput();
-//
-//    public void startGame(){
-//        Display.whatsYourName();
-//        String name = ui.getString();
-//        Display.howMuchMoney();
-//        int balance = ui.getInt();
-//        engine.createPlayer(name, balance);
-//    }
-//
-//    public void enterCasino(){
-//        Display.welcomeMessage();
-//        String answer;
-//        do{
-//            Display.requestGame();
-//            answer = ui.getString();
-//        }while (gameChoice(answer));
-//        Display.leaveCasino();
-//    }
-//
-//    public boolean gameChoice(String game){
-//        //regex
-//        try{
-//            switch (game.toUpperCase()){
-//                case "SLOTS": playSlots();break;
-//                case "BLACKJACK": playBlackjack();break;
-//                case "ROULETTE": playRoulette();break;
+
+    public void startGame(){
+        Display.whatsYourName();
+        String name = ui.getString();
+        Display.howMuchMoney();
+        int balance = ui.getInt();
+        engine.createPlayer(name, balance);
+    }
+
+    public void enterCasino(){
+        Display.welcomeMessage();
+        String answer;
+        do{
+            Display.requestGame();
+            answer = ui.getString();
+        }while (gameChoice(answer));
+        Display.leaveCasino();
+    }
+
+    public boolean gameChoice(String game){
+        //regex
+        try{
+            switch (game.toUpperCase()){
+                //case "SLOTS": playSlots();break;
+                //case "BLACKJACK": playBlackjack();break;
+                case "ROULETTE": playRoulette();break;
 //                case "AGRAM": playAgram();break;
 //                case "GO FISH": playGoFish();break;
 //                case "RUSSIAN ROULETTE": playRussianRoulette();break;
 //                case "BACCARAT": playBaccarat();break;
 //                case "WAR": playWar();break;
-//                case "QUIT": return false;
-//                default:
-//                    System.out.println("We did not understand your request, please try again.");return true;
-//            }
-//        }catch (InvalidBetException e){
-//            Display.weakBet();
-//        }catch (ZeroBalanceException z){
-//
-//        }catch (GameSizeException g){
-//
-//        }finally {
-//            return true;
-//        }
-//    }
-//
-//    private void playRoulette() {
-//        int bet;
-//        int betType;
-//        int numberGuess = 1;
-//        String response;
-//        Display.welcomeToRoulette();
-//        do{
-//
-//            Display.betType();
-//            betType=ui.getString();
-//            bet=requestBet();
-//            if(betType==4){
-//                numberGuess=ui.getInt();
-//            }
-//            Display.result(engine.playRoulette(bet,betType,numberGuess));
-//            Display.playAgain();
-//            response = ui.getString();
-//        }while (response.toUpperCase()=="YES");
-//    }
+                case "QUIT": return false;
+                default:
+                    System.out.println("We did not understand your request, please try again.");return true;
+            }
+        }catch (InvalidBetException e){
+            Display.weakBet();
+        //}catch (ZeroBalanceException z){
+
+        //}catch (GameSizeException g){
+
+        }finally {
+            return true;
+        }
+    }
+
+    private void playRoulette() {
+        int bet;
+        int betType;
+        int numberGuess = 1;
+        String response;
+        Display.rouletteWelcome();
+        do{
+
+            Display.rouletteAskBetType();
+            betType=ui.getInt();
+            bet=requestBet();
+            if(betType==4){
+                numberGuess=ui.getInt();
+            }
+            Display.result(engine.playRoulette(bet,betType,numberGuess));
+            //Display.playAgain();
+            response = ui.getString();
+        }while (response.toUpperCase()=="YES");
+    }
 //
 //    public void playSlots(){
 //        int bet;
@@ -138,11 +139,11 @@ public class Casino {
 //        }while (engine.gameRunning);
 //    }
 //
-//    public int requestBet(){
-//        Display.requestBet();
-//        return ui.getInt();
-//    }
-
+    public int requestBet(){
+        Display.requestBet();
+        return ui.getInt();
+    }
+//
 //    public static void main(String[] args) {
 //        Casino casino = new Casino();
 //
--- src/test/java/CasinoLab1Tests/AppTests.java
+++ src/test/java/CasinoLab1Tests/AppTests.java
@@ -4,4 +4,5 @@ package CasinoLab1Tests;
  * Created by joshuakelley on 10/12/16.
  */
 public class AppTests {
+
 }
