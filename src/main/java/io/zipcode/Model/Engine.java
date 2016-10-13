1ff2414e4ee255f0aed6a4dd383ddef2699521a3
Adam Kamsheh <adam@Adam-ZCW-2.local>
2016-10-13 14:04:04 -0400

    added 'playRoulette' method in engine

--- src/main/java/io/zipcode/Model/Engine.java
+++ src/main/java/io/zipcode/Model/Engine.java
@@ -1,5 +1,7 @@
 package io.zipcode.Model;
 
+import io.zipcode.View.Display;
+
 import java.util.ArrayList;
 
 /**
@@ -10,4 +12,12 @@ public class Engine {
     Roulette roulette;
 
     public ArrayList<Player> players = new ArrayList<>();//player info passed through casino,can manipulate without passing in/returning
+
+    public int playRoulette(int bet, int betType, int numberGuess) {
+        roulette = new Roulette(players);
+        roulette.placeBetType(0, betType);
+        roulette.askforNumber(0, numberGuess);
+        roulette.spinWheel();
+        return roulette.payoff();
+    }
 }
